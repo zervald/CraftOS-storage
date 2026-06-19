c7c8bdad748fbeb7d96ef630714ad522d5e8058b
Richard Demers <richardd1894@gmail.com>
2026-06-18 22:18:16 -0400

    check nil wrapped peri

--- src/artist/lib/turtle.lua
+++ src/artist/lib/turtle.lua
@@ -6,16 +6,22 @@ local function get_name()
   for _, side in ipairs(redstone.getSides()) do
     if peripheral.getType(side) == "modem" then
       local wrapped = peripheral.wrap(side)
+      if wrapped == nil then
+        error("Cannot wrap modem on side:" .. side, 0)
+      end
       local name = wrapped.getNameLocal and wrapped.getNameLocal()
-      if this_turtle then error("Cannot find turtle name: multiple modems", 0) end
+      if this_turtle then
+        error("Cannot find turtle name: multiple modems", 0)
+      end
       this_turtle = name
     end
   end
 
-  if this_turtle then return this_turtle end
+  if this_turtle then
+    return this_turtle
+  end
 
   error("Cannot find turtle name: none on the network", 0)
-
 end
 
 return { get_name = get_name }
